208eb8f0f51383054339cb73eabeb2620adee8f5
otenkaya <otenkaya@editsystems.com>
2020-08-12 17:21:04 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:AOtenkaya/Vue-Mastermind.git master:gh-pages
+git push -f git@github.com:AOtenkaya/Vue-Mastermind.git spare-to-components
 
 cd -
